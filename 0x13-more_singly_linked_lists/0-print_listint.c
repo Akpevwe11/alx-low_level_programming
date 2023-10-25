0664a680836e5d7b5bad1727c9dcaa227f6f13cb
praisex64 <essiakpevwepraise@gmail.com>
2023-10-24 21:37:12 -0700

    fix for task 0 again

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t counts = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		counts++;
+		nnodes++;
 	}
-	return (counts);
+	return (nnodes);
 }
