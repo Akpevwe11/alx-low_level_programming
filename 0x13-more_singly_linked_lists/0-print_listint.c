1cd6a422d466255dbe0b491070a21b10dc5dbd32
praisex64 <essiakpevwepraise@gmail.com>
2023-10-25 02:05:44 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,21 @@
 /**
  * print_listint - Print elements of a singly linked list.
  * @h: Pointer to a list.
+ *
  * Return: Number of nodes on the list.
  **/
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tp;
-	unsigned int cntr = 0;
+	const listint_t *temp;
+	unsigned int nodeCount = 0;
 
-	tp = h;
-	while (tp)
+	temp = h;
+	while (temp)
 	{
-		printf("%d\n", tp->n);
-		cntr++;
-		tp = tp->next;
+		printf("%d\n", temp->n);
+		nodeCount++;
+		temp = temp->next;
 	}
-	return (cntr);
+	return (nodeCount);
 }
