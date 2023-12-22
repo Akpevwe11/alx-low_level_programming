3ef372823311e3a185fe849a25e67aaf03d54472
praisex64 <essiakpevwepraise@gmail.com>
2023-12-22 11:28:01 -0800

    fix for task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -25,9 +25,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-	return (node->value);
+	return ((node == NULL) ? NULL : node->value);
 }
