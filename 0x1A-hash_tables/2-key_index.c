69e89ca3a183be4e800e4b247c6df39bb062b168
praisex64 <essiakpevwepraise@gmail.com>
2023-12-22 09:54:14 -0800

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Calculate the index at which a key/value
+ * pair should be stored in the array of a hash table
+ * @key: The key for which to calculate the index
+ * @size: The size of the array in the hash table
+ *
+ * Return: The index of the key.
+ *
+ * Description: This function uses the djb2 algorithm
+ * to determine the index based on the hash value of the key.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value = hash_djb2(key);
+
+	unsigned long int index = hash_value % size;
+
+	return (index);
+}
